1beca158acd5e034019dad0d7a380cb1490d7387
LIlia <lilia_dyrnina@mail.ru>
2023-10-24 12:42:27 +0300

    label,button,entry

--- form.py
+++ form.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 
 root = tk.Tk()
-root.title("Титульник")
+root.title("Добро пожаловать в КНТ-6")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -17,3 +17,12 @@ root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 root.mainloop()    
 
+label1 = tk.Label(root, text="Привет!",font="Courier 20",bg="pink")
+label1.place(x=100, y=100)
+label2 = tk.Label(root, text="Введи дату своего рождения",font="Courier 15",bg="pink")
+label2.place(x=150, y=150)
+entry=tk.Entry(root,font=" 10")
+entry.place(x=150,y=180)
+
+button1 = tk.Button(root, text="ok",font="Courier 10",command=button1_click)
+button1.place(x=355, y=180)
