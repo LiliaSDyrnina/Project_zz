a134725d5477192b2251cd405e3a8a3511cff9f2
LIlia <lilia_dyrnina@mail.ru>
2023-10-25 17:18:54 +0300

    add info

--- form.py
+++ form.py
@@ -1,13 +1,16 @@
-<<<<<<< HEAD
 from tkinter import *
 import tkinter as tk
-
+from textwrap import wrap
 
 root = tk.Tk()
 root.title("Добро пожаловать в КНТ-6")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
+root.config(cursor="sailboat")
+
+root.configure(bg='white')
+
 
 
 window_width = 500
@@ -16,78 +19,236 @@ x = (screen_width // 2) - (window_width // 2)
 y = (screen_height // 2) - (window_height // 2)
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-label1 = tk.Label(root, text="Привет!",font="Courier 20",bg="pink")
+label1 = tk.Label(root, text="Здравствуй!",font="TkHeadingFont 17",bg="white")
 label1.place(x=100, y=100)
-label2 = tk.Label(root, text="Введи дату своего рождения",font="Courier 15",bg="pink")
+label2 = tk.Label(root, text="Введи дату рождения",font="Courier 15",bg="white")
 label2.place(x=150, y=150)
 entry=tk.Entry(root,font="Courier 13")
 entry.place(x=150,y=180)
 
+
+
+ 
 def button1_click():
-    date=entry.get()
-    label3 = tk.Label(root, text="Вы",font="Courier 15",bg="pink")
+    data=str(entry.get())
+    label3 = tk.Label(root, text="Вы",font="Courier 19",bg="white")
     label3.place(x=150, y=220)
-    buttonw = tk.Button(root, text="женщина",font="Courier 10",bg="pink",command=buttonw_click)
-    buttonw.place(x=210, y=220)
-    buttonm = tk.Button(root, text="мужчина",font="Courier 10",bg="blue",command=buttonm_click)
-    buttonm.place(x=290, y=220)
+    buttonw = tk.Button(root, text="женщина",font="Courier 13",bg="blue",command=buttonw_click)
+    buttonw.place(x=206, y=220)
+    buttonm = tk.Button(root, text="мужчина",font="Courier 13",bg="red",command=buttonm_click)
+    buttonm.place(x=295, y=220)
+    return data
     
-button1 = tk.Button(root, text="ok",font="Courier 10",command=button1_click)
+button1 = tk.Button(root, text="ok",font="Courier 10",bg="white", command=button1_click)
 button1.place(x=355, y=180)
-def go_click():
-    parameters=open('info.txt').readlines()
-    dates=[]
-    sexes=[]
-    names=[]
-    signs=[]
-    captions=[]
-    connections=[]
-    for i in range(0,len(parameters)):
-        if i%6==0:
-            dates+=[''.join(parameters[i][0:-1:])]
-        if i%6==1:
-            sexes+=[''.join(parameters[i][0:-1:])]
-        if i%6==2:
-            names+=[''.join(parameters[i][0:-1:])]
-        if i%6==3:
-            signs+=[''.join(parameters[i][0:-1:])]
-        if i%6==4:
-            captions+=[''.join(parameters[i][0:-1:])]
-        if i%6==5:
-            connections+=[''.join(parameters[i][0:-1:])]
-    if data in dates: 
-        if sex=='ж':
-            for i in range(len(dates)):
-                if dates[i]==data:
-                    index=i
-                    break
-        if sex=='м':
-            for i in range(len(dates)):
-                if dates[i]==data:
-                    index=i
-        root.destroy()
-        import good
-        label1=''.join(('Привет, ',names[index],'!'))
-        label2=''.join(('Твой знак зодиака: ', signs[index]))
-        label3=''.join(('Твои личностные качества: ', captions[index]))
-        label4=''.join(('В нашей группе у тебя ты cможешь построить доверительные отношения с ', connections[index]))
-    else:
-        root.destroy()
-        import bug
-
-
-
 def buttonw_click():
     sex='ж'
-    button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
-    button_go.place(x=100, y=266)
+    def go_click():
+        parameters=open('info.txt','r',encoding='utf-8').readlines()
+        dates=[]    
+        data=(entry.get())        
+        sexes=[]
+        names=[]
+        signs=[]
+        captions=[]
+        connections=[]
+        for i in range(0,len(parameters)):
+            if i%6==0:
+                dates+=[''.join(parameters[i][0:-1:])]
+            if i%6==1:
+                sexes+=[''.join(parameters[i][0:-1:])]
+            if i%6==2:
+                names+=[''.join(parameters[i][0:-1:])]
+            if i%6==3:
+                signs+=[''.join(parameters[i][0:-1:])]
+            if i%6==4:
+                captions+=[''.join(parameters[i][0:-1:])]
+            if i%6==5:
+                connections+=[''.join(parameters[i][0:-1:])]
+        if data in dates: 
+            if sex=='ж':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+                        break
+            if sex=='м':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+            
+            for widget in root.place_slaves():
+                widget.place_forget()
+            
+            
+            label1=''.join(('Привет, ',names[index],'!'))
+            
+            label_1 = tk.Label(root, text=label1,font="Courier 15",cursor="pencil",bg='white')            
+            label_1.place(x=180, y=20)
+            root.configure(bg='white')
+            
+                   
 
+            
+            labe1=''.join(('Привет, ',names[index],'!'))
+            
+            
+            label2=''.join(('Твой знак зодиака: ', signs[index]))
+            label_2 = tk.Label(root, text=label2,font="Courier 13",bg="#efdecd")
+            label_2.place(x=10, y=70)
+            
+            
+            
+            label3_=''.join((captions[index]))
+                    
+            
+            label_9 = tk.Label(root, text="Личностные качества: ",font="Courier 13",bg="#c0c0c0")
+            label_9.place(x=10, y=110)
+            label3=''.join(captions[index])
+                        
+            
+            text=label3_
+            label = Label(root, text=text,font="Courier 13",bg="white",anchor="e")
+            label.place(x=10,y=140)       
+            root.update()  
+
+            width = label.winfo_width()
+
+            if width > 475:
+                char_width = width / len(text)
+                wrapped_text = '\n'.join(wrap(text, int(475 / char_width)))
+                label['text'] = wrapped_text
+            label4_=''.join((connections[index]))
+            label_10 = tk.Label(root, text="У тебя хорошая совместимость с этими людьми:",font="Courier 13",bg="#b2cde8")
+            label_10.place(x=10, y=375)
+                
+            text=label4_
+            label = Label(root, text=text,font="Courier 12",bg="white",cursor="gumby")
+            label.place(x=10,y=400)       
+            root.update()  
+
+            width = label.winfo_width()
+
+            if width > 475:
+                char_width = width / len(text)
+                wrapped_text = '\n'.join(wrap(text, int(475/ char_width)))
+                label['text'] = wrapped_text
+            
+          
+           
+        else:
+            
+            root.destroy()
+            import bug
+
+
+    button_go = tk.Button(root, text="Рассчитать",font="Courier 15",bg="#c8b2d8",command=go_click)
+    button_go.place(x=100, y=266)
     
+
 def buttonm_click():
     sex='м'
-    button_go = tk.Button(root, text="Рассчитать",font="Courier 15",command=go_click)
-    button_go.place(x=100, y=266)
+    def go_click():
+        parameters=open('info.txt','r',encoding='utf-8').readlines()
+        dates=[]    
+        data=(entry.get())        
+        sexes=[]
+        names=[]
+        signs=[]
+        captions=[]
+        connections=[]
+        for i in range(0,len(parameters)):
+            if i%6==0:
+                dates+=[''.join(parameters[i][0:-1:])]
+            if i%6==1:
+                sexes+=[''.join(parameters[i][0:-1:])]
+            if i%6==2:
+                names+=[''.join(parameters[i][0:-1:])]
+            if i%6==3:
+                signs+=[''.join(parameters[i][0:-1:])]
+            if i%6==4:
+                captions+=[''.join(parameters[i][0:-1:])]
+            if i%6==5:
+                connections+=[''.join(parameters[i][0:-1:])]
+        if data in dates: 
+            if sex=='ж':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+                        break
+            if sex=='м':
+                for i in range(len(dates)):
+                    if dates[i]==data:
+                        index=i
+            
+            for widget in root.place_slaves():
+                widget.place_forget()
+            
+            
+            label1=''.join(('Привет, ',names[index],'!'))
+            
+            label_1 = tk.Label(root, text=label1,font="Courier 15",cursor="pencil",bg='white')            
+            label_1.place(x=20, y=20)
+            root.configure(bg='white')
+            
+                   
 
+            
+            labe1=''.join(('Привет, ',names[index],'!'))
+            
+            
+            label2=''.join(('Твой знак зодиака: ', signs[index]))
+            label_2 = tk.Label(root, text=label2,font="Courier 13",bg="#efdecd")
+            label_2.place(x=10, y=70)
+            
+            
+            
+            label3_=''.join((captions[index]))
+                    
+            
+            label_9 = tk.Label(root, text="Личностные качества: ",font="Courier 13",bg="#c0c0c0")
+            label_9.place(x=10, y=110)
+            label3=''.join(captions[index])
+                        
+            
+            text=label3_
+            label = Label(root, text=text,font="Courier 13",bg="white",anchor="e")
+            label.place(x=10,y=140)       
+            root.update()  
+
+            width = label.winfo_width()
+
+            if width > 475:
+                char_width = width / len(text)
+                wrapped_text = '\n'.join(wrap(text, int(475 / char_width)))
+                label['text'] = wrapped_text
+            label4_=''.join((connections[index]))
+            label_10 = tk.Label(root, text="У тебя хорошая совместимость с этими людьми:",font="Courier 13",bg="#b2cde8")
+            label_10.place(x=10, y=375)
+                
+            text=label4_
+            label = Label(root, text=text,font="Courier 12",bg="white",cursor="gumby")
+            label.place(x=10,y=400)       
+            root.update()  
+
+            width = label.winfo_width()
+
+            if width > 475:
+                char_width = width / len(text)
+                wrapped_text = '\n'.join(wrap(text, int(475/ char_width)))
+                label['text'] = wrapped_text
+            
+          
+           
+        else:
+            
+            root.destroy()
+            import bug
+
+
+    button_go = tk.Button(root, text="Рассчитать",font="Courier 15",bg="#c8b2d8",command=go_click)
+    button_go.place(x=100, y=266)
+       
 
 
 root.mainloop()    
+
